a006beac244434b9c9ce5fdce3d56aeaaf224bd2
Helton Brito <heltoncarlospa@hotmail.com>
2023-01-12 22:59:28 -0300

    feat: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#F21905",
+      white: "#fff",
+      green: "#5ed5a8",
+      yellow: "#ffc82c",
+      gray: "#999fae",
+      "gray-dark": "#273444",
+      "gray-light": "#d3dce6",
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  plugins: [],
+};
